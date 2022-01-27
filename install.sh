d57962fd9b969f28200a3743016bcd56d6a9ee59
Homero Bonomini <homero@pixelti.com.br>
2022-01-27 10:13:15 -0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,165 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+GRUB_THEME='retro-grub-theme'
+INSTALLER_LANG='English'
+
+# Check dependencies
+INSTALLER_DEPENDENCIES=(
+    'mktemp'
+    'sed'
+    'sort'
+    'sudo'
+    'tar'
+    'tee'
+    'tr'
+    'wget'
+)
+
+for i in "${INSTALLER_DEPENDENCIES[@]}"; do
+    command -v $i > /dev/null 2>&1 || {
+        echo >&2 "'$i' command is required, but not available. Aborting.";
+        exit 1;
+    }
+done
+
+source=$PWD
+# Change to temporary directory
+cd $(mktemp -d)
+
+# Pre-authorise sudo
+sudo echo
+
+# Select language, optional
+declare -A INSTALLER_LANGS=(
+    [Chinese]=zh_CN
+    [English]=EN
+    [French]=FR
+    [German]=DE
+    [Italian]=IT
+    [Norwegian]=NO
+    [Portuguese]=PT
+    [Russian]=RU
+    [Spanish]=ES
+    [Turkish]=TR
+    [Ukrainian]=UA
+)
+
+INSTALLER_LANG_NAMES=($(echo ${!INSTALLER_LANGS[*]} | tr ' ' '\n' | sort -n))
+
+PS3='Please select language #: '
+#select l in "${INSTALLER_LANG_NAMES[@]}"; do
+
+l=English
+if [[ -v INSTALLER_LANGS[$l] ]]; then
+    INSTALLER_LANG=$l
+else
+    echo 'No such language, fallback to English.'
+    INSTALLER_LANG=English
+fi
+#  break
+#done < /dev/tty
+
+#echo 'Fetching and unpacking theme'
+#wget -O - https://github.com/shvchk/${GRUB_THEME}/archive/master.tar.gz | tar -xzf - --strip-components=1
+cp -r $source/{theme.txt,background.png,icons,item*,selected_item*,terminal_box*,*.pf2} .
+
+if [[ "$INSTALLER_LANG" != "English" ]]; then
+    echo "Changing language to ${INSTALLER_LANG}"
+    sed -i -r -e '/^\s+# EN$/{n;s/^(\s*)/\1# /}' \
+              -e '/^\s+# '"${INSTALLER_LANGS[$INSTALLER_LANG]}"'$/{n;s/^(\s*)#\s*/\1/}' theme.txt
+fi
+
+# Detect distro and set GRUB location and update method
+GRUB_DIR='grub'
+UPDATE_GRUB=''
+BOOT_MODE='legacy'
+
+if [[ -d /boot/efi && -d /sys/firmware/efi ]]; then
+    BOOT_MODE='UEFI'
+fi
+
+echo "Boot mode: ${BOOT_MODE}"
+
+if [[ -e /etc/os-release ]]; then
+
+    ID=""
+    ID_LIKE=""
+    source /etc/os-release
+
+    if [[ "$ID" =~ (debian|ubuntu|solus|void) || \
+          "$ID_LIKE" =~ (debian|ubuntu|void) ]]; then
+
+        UPDATE_GRUB='update-grub'
+
+    elif [[ "$ID" =~ (arch|gentoo) || \
+            "$ID_LIKE" =~ (archlinux|gentoo) ]]; then
+
+        UPDATE_GRUB='grub-mkconfig -o /boot/grub/grub.cfg'
+
+    elif [[ "$ID" =~ (centos|fedora|opensuse) || \
+            "$ID_LIKE" =~ (fedora|rhel|suse) ]]; then
+
+        GRUB_DIR='grub2'
+        GRUB_CFG='/boot/grub2/grub.cfg'
+
+        if [[ "$BOOT_MODE" = "UEFI" ]]; then
+            GRUB_CFG="/boot/efi/EFI/${ID}/grub.cfg"
+        fi
+
+        UPDATE_GRUB="grub2-mkconfig -o ${GRUB_CFG}"
+
+        # BLS etries have 'kernel' class, copy corresponding icon
+        if [[ -d /boot/loader/entries && -e icons/${ID}.png ]]; then
+            cp icons/${ID}.png icons/kernel.png
+        fi
+    fi
+fi
+
+THEME_DIR=/boot/${GRUB_DIR}/themes/${GRUB_THEME}
+
+echo 'Removing GRUB theme directory from previous installation'
+sudo rm -rf $THEME_DIR
+
+echo 'Creating GRUB theme directory'
+sudo mkdir -p $THEME_DIR
+
+echo 'Copying theme to GRUB themes directory'
+sudo cp -r * $THEME_DIR
+
+echo 'Removing other themes from GRUB config'
+sudo sed -i '/^GRUB_THEME=/d' /etc/default/grub
+
+echo 'Making sure GRUB uses graphical output'
+sudo sed -i 's/^\(GRUB_TERMINAL\w*=.*\)/#\1/' /etc/default/grub
+
+echo 'Removing empty lines at the end of GRUB config' # optional
+sudo sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' /etc/default/grub
+
+echo 'Adding new line to GRUB config just in case' # optional
+echo | sudo tee -a /etc/default/grub
+
+echo 'Adding theme to GRUB config'
+echo "GRUB_THEME=$THEME_DIR/theme.txt" | sudo tee -a /etc/default/grub
+
+echo 'Removing theme installation files'
+rm -rf "$PWD"
+cd
+
+echo 'Updating GRUB'
+if [[ $UPDATE_GRUB ]]; then
+    eval sudo "$UPDATE_GRUB"
+else
+    cat << '    EOF'
+    --------------------------------------------------------------------------------
+    Cannot detect your distro, you will need to run `grub-mkconfig` (as root) manually.
+
+    Common ways:
+    - Debian, Ubuntu, Solus and derivatives: `update-grub` or `grub-mkconfig -o /boot/grub/grub.cfg`
+    - RHEL, CentOS, Fedora, SUSE and derivatives: `grub2-mkconfig -o /boot/grub2/grub.cfg`
+    - Arch, Gentoo and derivatives: `grub-mkconfig -o /boot/grub/grub.cfg`
+    --------------------------------------------------------------------------------
+    EOF
+fi
